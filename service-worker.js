09a8ae31798553a89a8dd63abcefd0663a1fb7fd
tvs-apps <tvansantwork@gmail.com>
2025-10-19 11:50:42 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // A version number is used to bust the cache when you deploy new updates.
-const CACHE_NAME = 'yahtzee-scorekeeper-v1';
+const CACHE_NAME = 'yahtzee-scorekeeper-v1.1';
 
 // A list of files to cache for offline use.
 const FILES_TO_CACHE = [
